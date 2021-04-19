e1c2e35153b57ea3c4f4107291b304f1cf134f41
Nishat2016831010 <42400265+NaziaNishat@users.noreply.github.com>
2021-04-19 12:39:43 +0600

    Add Layout

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Auxx from '../../hoc/Auxx';
+
+const layout = ( props ) => (
+    <Auxx>
+        <div>Toolbar, SideDrawer, Backdrop</div>
+        <main>
+            {props.children}
+        </main>
+    </Auxx>
+);
+
+export default layout;
