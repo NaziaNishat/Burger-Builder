6d8cc18796cc7bf1d4807df08105b6bacbffc4c8
Nishat2016831010 <42400265+NaziaNishat@users.noreply.github.com>
2021-04-19 16:48:20 +0600

    Render Burger Ingredient Dynamically

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -42,7 +42,7 @@ class BurgerIngredient extends Component {
 }
 
 
-BurgerIngredient.PropTypes = {
+BurgerIngredient.propTypes = {
     type: PropTypes.string.isRequired
 };
 export default BurgerIngredient;
