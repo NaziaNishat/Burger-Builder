aecc0ddfd174e57a114e23492addd7f2e0a88c0c
Nishat2016831010 <42400265+NaziaNishat@users.noreply.github.com>
2021-04-29 15:39:37 +0600

    Add SideDrawer Toggl Button

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -7,17 +7,23 @@ import SideDrawer from '../Navigation/Sidedrawer/SideDrawer';
 class Layout extends Component {
 
     state = {
-        showSideDrawer: true
+        showSideDrawer: false
     }
 
     SideDrawerClosedHandler = () => {
         this.setState({showSideDrawer: false});
     }
 
+    SideDrawerTogglHandler = () => {
+        this.setState(( prevState ) => {
+            return {showSideDrawer: !prevState.showSideDrawer};
+        });
+    }
+
     render() {
         return (
             <Auxx>
-                <Toolbar />
+                <Toolbar drawerTogglClicked={this.SideDrawerTogglHandler}/>
                 <SideDrawer open={this.state.showSideDrawer} closed={this.SideDrawerClosedHandler}/>
                 <main className={classes.Content}>
                     {this.props.children}
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,10 +2,11 @@ import React from 'react';
 import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggl from '../Sidedrawer/DrawerToggl/DrawerToggl';
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
-        <div>Menu</div>
+        <DrawerToggl clicked={props.drawerTogglClicked}/>
         <div className={classes.Logo}>
             <Logo />
         </div>
