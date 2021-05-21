33638e7055860c53029d377ee01a6fa723b94859
Nishat2016831010 <42400265+NaziaNishat@users.noreply.github.com>
2021-05-21 12:16:49 +0600

    Adjusting Checkout n Contact Data

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -4,6 +4,7 @@ import classes from './ContactData.css';
 import axios from '../../../axios-orders';
 import Spinner from "../../../components/UI/Spinner/Spinner";
 import Input from '../../../components/UI/Input/Input';
+import { connect } from "react-redux";
 
 
 class ContactData extends Component {
@@ -105,7 +106,7 @@ class ContactData extends Component {
         }
 
         const order = {
-            ingredients: this.props.ingredients,
+            ingredients: this.props.ings,
             // price is normally set up on the SERVER so that a customer cannot alter it!!!
             price: this.props.price,
             orderData: formData
@@ -201,4 +202,11 @@ class ContactData extends Component {
 
 }
 
-export default ContactData;
+const mapStateToProps = state => {
+    return {
+        ings: state.ingredients,
+        price: state.totalPrice
+    }
+};
+
+export default connect(mapStateToProps)(ContactData);
