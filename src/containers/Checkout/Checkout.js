b939a1aa1323e5f4770b78d6ce5ff4bb9555d820
Nishat2016831010 <42400265+NaziaNishat@users.noreply.github.com>
2021-05-23 11:00:00 +0600

    Handling Purchases & Updating the UI

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -3,8 +3,13 @@ import { connect } from 'react-redux';
 import { Redirect, Route } from 'react-router-dom';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import ContactData from './ContactData/ContactData';
+import * as actions from '../../store/actions/index';
 
 class Checkout extends Component {
+
+    componentWillMount() {
+        this.props.onInitPurchase();
+    }
 
     checkoutCancelledHandler = () => {
         this.props.history.goBack();
@@ -16,9 +21,11 @@ class Checkout extends Component {
 
     render() {
         let summary = <Redirect to="/" />
-        if (this.props.ings) [
+        if (this.props.ings) {
+            const purchasedRedirect = this.props.purchased ? <Redirect to='/' />: null;
             summary = (
                 <div>
+                    {purchasedRedirect}
                     <CheckoutSummary
                         ingredients={this.props.ings}
                         checkoutCancelled={this.checkoutCancelledHandler}
@@ -28,7 +35,7 @@ class Checkout extends Component {
                         component={ContactData} />
                 </div>
             )
-        ]
+        }
 
         return summary;
     }
@@ -36,8 +43,15 @@ class Checkout extends Component {
 
 const mapStateToProps = state => {
     return {
-        ings: state.burgerBuilder.ingredients
+        ings: state.burgerBuilder.ingredients,
+        purchased: state.order.purchased
     };
 };
 
-export default connect(mapStateToProps)(Checkout);
+const mapDispatchToProps = dispatch => {
+    return {
+        onInitPurchase: () => dispatch(actions.purchaseInit())
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Checkout);
