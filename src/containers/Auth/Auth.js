e7083fa96157cd70b324e23b65f12cc99bd3ddeb
Nishat2016831010 <42400265+NaziaNishat@users.noreply.github.com>
2021-05-28 07:06:43 +0600

    Forwarding Unauthenticated Users

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -5,6 +5,7 @@ import Input from '../../components/UI/Input/Input';
 import classes from './Auth.css';
 import * as actions from '../../store/actions/index';
 import { connect } from 'react-redux'
+import { Redirect } from "react-router";
 
 class Auth extends Component {
 
@@ -129,8 +130,14 @@ class Auth extends Component {
             );
         }
 
+        let authRedirect = null;
+        if(this.props.isAuthenticated) {
+            authRedirect = <Redirect to="/"/>
+        }
+
         return (
             <div className={classes.Auth}>
+                {authRedirect}
                 {errorMessage}
                 <form onSubmit={this.submitHandler}>
                     {form}
@@ -146,7 +153,8 @@ class Auth extends Component {
 const mapStateToProps = state => {
     return {
         loading: state.auth.loading,
-        error: state.auth.error
+        error: state.auth.error,
+        isAuthenticated: state.auth.token !== null
     };
 };
 
